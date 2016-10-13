f273fb341cf4c2b93ebda1c38983dbe8cdc86da9
Jamie Scott <github@jami.org.uk>
2016-10-13 20:23:21 +0100

    completed encode

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
@@ -18,15 +25,72 @@ public class Main {
              * ENCODE
              */
 
-            // Read text and add stop sequence
+            // Read text
+            System.out.print("String to encode: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input;
+            input = null;
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                System.err.println("There was an input error");
+            }
 
-            // Convert to binary string
+            // Convert to binary string and add stop sequence
+            String binaryStr = "";
+            for (int i = 0; i < input.getBytes().length; i++) {
+                binaryStr += Integer.toBinaryString(input.getBytes()[0]);
+            }
+            binaryStr += "10101010101010101010101010101010"; // 32bit long stop sequence
+            char[] bin = binaryStr.toCharArray();
 
             // Read image
+            BufferedImage img = null;
+            try {
+                img = ImageIO.read(new File("image.jpg"));
+            } catch (IOException e) {
+                System.err.println("Error reading image");
+            }
 
-            // Set LSB of pixels to binary string
+            if (img != null) {
+                // Set LSB of pixels to binary string
+                int count = 0;
+                for (int y = 0; y < img.getHeight(); y++) {
+                    for (int x = 0; x < img.getWidth(); x++) {
+                        Color color = new Color(img.getRGB(x, y));
+                        int r = color.getRed();
+                        int g = color.getGreen();
+                        int b = color.getBlue();
+                        String binVal = Integer.toBinaryString(r);
+                        if (binVal.length() < 8) {
+                            String add = "";
+                            for (int k = 0; k < (8 - binVal.length()); k++) {
+                                add += "0";
+                            }
+                            binVal += add;
+                        }
+                        //System.out.println(binVal);
+                        String outStr = binVal.substring(0, binVal.length() - 1) + bin[count];
+                        count++;
+                        r = Integer.parseInt(outStr, 2);
+                        img.setRGB(x, y, (new Color(r, g, b)).getRGB());
+                        if (count >= bin.length) {
+                            break;
+                        }
+                    }
+                    if (count >= bin.length) {
+                        break;
+                    }
+                }
 
-            // Save image
+                // Save image
+                try {
+                    File outputfile = new File("output.jpg");
+                    ImageIO.write(img, "jpg", outputfile);
+                } catch (IOException e) {
+                    System.err.println("Error writing image to file.");
+                }
+            }
         } else if (n == 2) {
             /*
              * DECODE
