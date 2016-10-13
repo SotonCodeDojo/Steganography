b18cd0ba829cf5e2ab9dd7230027e06e9a1af82f
Jamie Scott <github@jami.org.uk>
2016-10-13 19:41:11 +0100

    added tasks

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        /*
+         * ENCODE
+         */
+
+        // Read text and add stop sequence
+
+        // Convert to binary string
+
+        // Read image
+
+        // Set LSB of pixels to binary string
+
+        // Save image
+
+        /*
+         * DECODE
+         */
+
+        // Read image
+
+        // Strip binary values out and trim bits after stop sequence
+
+        // Convert binary to text
+
+        // Output text
     }
 }
